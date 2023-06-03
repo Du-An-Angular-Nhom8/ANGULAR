ae62357a7309eaa648b2f0d181c2dab9e552e471
quan1234 <quanvdph19781@fpt.edu.vn>
2023-06-04 03:27:50 +0700

    GetAll Product -GetAllPro refs:WA-48

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 import { DashbordComponent } from './component/admin/dashbord/dashbord.component';
+import { ListProductComponent } from './component/admin/list-product/list-product.component';
 
 const routes: Routes = [
   {path: 'admin', component: AdminLayoutComponent,children:[
     {path: '', redirectTo: 'dashbord', pathMatch:'full'},
-    {path: 'dashbord', component:DashbordComponent}
+    {path: 'dashbord', component:DashbordComponent},
+      { path: 'products', component: ListProductComponent }
   ]}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 import { DashbordComponent } from './component/admin/dashbord/dashbord.component';
+import { ListProductComponent } from './component/admin/list-product/list-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    DashbordComponent
+    DashbordComponent,
+    ListProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
