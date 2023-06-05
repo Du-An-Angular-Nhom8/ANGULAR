20c6b0d611583029aca467692603e16f72164066
quan1234 <quanvdph19781@fpt.edu.vn>
2023-06-06 05:11:11 +0700

    Create Add Product -UI Add Product -Upload file refs: WA-51

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,19 @@ import { DashbordComponent } from './component/admin/dashbord/dashbord.component
 import { ListProductComponent } from './component/admin/list-product/list-product.component';
 import { ListCategoryComponent } from './component/admin/list-category/list-category.component';
 import { AddCategoriesComponent } from './component/admin/add-categories/add-categories.component';
+import { AddProductComponent } from './component/admin/add-product/add-product.component';
 
 const routes: Routes = [
-  {path: 'admin', component: AdminLayoutComponent,children:[
-    {path: '', redirectTo: 'dashbord', pathMatch:'full'},
-    {path: 'dashbord', component:DashbordComponent},
+  {
+    path: 'admin', component: AdminLayoutComponent, children: [
+      { path: '', redirectTo: 'dashbord', pathMatch: 'full' },
+      { path: 'dashbord', component: DashbordComponent },
       { path: 'products', component: ListProductComponent },
-    { path: 'category', component: ListCategoryComponent },
-    { path: 'category/add', component: AddCategoriesComponent }
-  ]}
+      { path: 'products/add', component: AddProductComponent },
+      { path: 'category', component: ListCategoryComponent },
+      { path: 'category/add', component: AddCategoriesComponent }
+    ]
+  }
 ];
 
 @NgModule({
