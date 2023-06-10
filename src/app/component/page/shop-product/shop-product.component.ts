e8d663f58b66d3edd342aa5f3c4504c3f1b3d6aa
quan1234 <quanvdph19781@fpt.edu.vn>
2023-06-10 16:58:39 +0700

    bug cart

--- src/app/component/page/shop-product/shop-product.component.ts
+++ src/app/component/page/shop-product/shop-product.component.ts
@@ -17,6 +17,14 @@ export class ShopProductComponent {
   dataCart: any = {
     _id: ''
   }
+ 
+  password: any = ''
+  user: any = {}
+  showPasswordInput: boolean = true;
+  datalogin: any = {
+    email: '',
+    password: '',
+  }
   initialProducts: any = [];
 
   constructor(private productService: ProductService, private cartService: CartService, private router: Router, private authService: AuthService, private userService: UserService) {
@@ -31,6 +39,47 @@ export class ShopProductComponent {
     const accessToken = user ? user.accessToken : undefined;
     const idUser = user && user.user ? user.user._id : undefined;
     this.dataCart._id = id;
+    const carrt = user && user.user ? user.user.cart : undefined
+    this.dataCart._id = id
+    if (!carrt) {
+      // alert("okok")
+      // ... các thao tác khác
+
+      const pass: any = prompt('Hãy nhập mật khẩu để tạo giỏ hàng')
+      if (pass !== null) {
+        console.log(pass); // In ra giá trị người dùng đã nhập khi nhấn OK
+        this.password = pass
+        // Thực hiện xử lý với giá trị pass ở đây
+        console.log(user);
+        this.user = user
+        const idUser = user && user.user ? user.user._id : undefined;
+        // console.log(id);
+        
+        
+        this.userService.getOneUser(idUser).subscribe((data: any) => {
+          this.datalogin.email = data.email
+          this.datalogin.password = this.password
+
+          // localStorage.removeItem('user')
+          // localStorage.setItem('user', JSON.stringify(data))
+          console.log(this.datalogin);
+          this.cartService.AddToCart(this.dataCart).subscribe(data => {
+            console.log('addtocart thanh cong');
+          })
+          this.authService.Signin(this.datalogin).subscribe(data => {
+            console.log(data);
+            localStorage.setItem('user', JSON.stringify(data))
+          })
+          return
+        })
+        return
+      } else {
+        console.log('Bạn đã bỏ qua nhập mật khẩu'); // In ra khi người dùng nhấn Cancel
+        return
+        // Thực hiện xử lý khi người dùng bỏ qua nhập mật khẩu ở đây
+      }
+      this.showPasswordInput = false;
+    }
     this.cartService.AddToCart(this.dataCart).subscribe(data => {
       console.log('addtocart thanh cong');
       const ok = confirm('Bạn đã thêm sản phẩm vào giỏ hàng. Hãy kiểm tra !')
